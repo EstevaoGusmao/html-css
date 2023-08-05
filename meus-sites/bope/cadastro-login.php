c38abeafc10d0528a7b0ccbe9ca7e5c050b12900
Estêvão Gusmão <tevogusmao@gmail.com>
2023-08-04 23:30:30 -0300

    V2

---
+++ meus-sites/bope/cadastro-login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BOPE</title>
+    <style>
+        body {
+            font-family: Arial, Helvetica, sans-serif;
+            color: red;
+            background: linear-gradient(to right, rgb(20, 147, 220), rgb(17, 54, 71));
+        }
+    </style>
+</head>
+<body>
+    <h1>Se inscreva no canal</h1>
+    <h1>Developer Gusmão</h1>
+    <div class="box">
+        <a href="#">Login</a>
+        <a href="#">Cadastro</a>
+    </div>
+</body>
+</html>
